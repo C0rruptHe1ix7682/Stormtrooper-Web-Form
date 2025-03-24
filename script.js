82cc0730179ef1ab8907850e6584fe58718cc03f
NateLawrence-w <natewolf676@gmail.com>
2025-03-24 09:03:22 -0400

    fanal part 2

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.getElementById('Stormtrooper-web-form button').addEventListener('submit
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
  
-    alert('Feedback submitted successfully!');
+   alert('Feedback submitted successfully!');
 
    
  
@@ -21,10 +21,10 @@ document.getElementById('Stormtrooper-web-form button').addEventListener('submit
    submissionCount = parseInt(submissionCount) + 1;
 
 if (submissionCount <=5) { localStorage.setItem('submissionCount', submissionCount);
-      alert(', submited ')
+      alert(' submited ');
 
   //this disables the submit buttn after 5 submissions
- if (submissionCount >=5) {
+if (submissionCount >=5) {
    alert("You have reached the submission limit (5 times). You cannot submit again.");
    document.querySelector('button').disabled = true; 
 }
